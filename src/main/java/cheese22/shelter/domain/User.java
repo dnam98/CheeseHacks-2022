1a03b76992350d5a546ca83204c0293ec965681a
Dongwon <dongwonnam98@gmail.com>
2022-11-12 14:11:43 -0600

    User domain

--- src/main/java/cheese22/shelter/domain/User.java
+++ src/main/java/cheese22/shelter/domain/User.java
@@ -1,5 +1,76 @@
 package cheese22.shelter.domain;
 
 public class User {
+    private String first_name;
+    private String last_name;
+    private String email;
+    private int phone;
+    private String id;
+    private String pw;
+    private String location;
+    private String status;
 
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
